5998d2da0df216a999c938e010fe56c99ba4a367
anibalrojosan <anibal.rojo.sandoval@gmail.com>
2026-03-25 16:47:52 -0300

    chore: improve User profiles viz in Django Admin

--- profiles/models.py
+++ profiles/models.py
@@ -6,6 +6,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(max_length=500, blank=True)
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Profile for {self.user.username}"
